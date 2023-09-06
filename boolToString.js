f41dd0f285578930ab7fbd3083b7a581de6a703b
Menyene Thomas <56399785+miraclemoses@users.noreply.github.com>
2023-09-06 03:31:55 +0100

    Create boolToString.js

---
+++ boolToString.js
@@ -0,0 +1,8 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+//solution
+function boolToString(b) {
+     return b.toString()
+}
+
+boolToString(true)
