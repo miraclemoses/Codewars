a6ec761b9532d6efa7bddbbb8eea3f738fd9c0be
Menyene Thomas <56399785+miraclemoses@users.noreply.github.com>
2023-09-10 04:09:20 +0100

    Update catDog.js

--- catDog.js
+++ catDog.js
@@ -14,14 +14,14 @@ function humanYearsCatYearsDogYears(humanYears) {
   let catYears = 0;
   let dogYears = 0;
   
-  for(let i = 1; i <= humanYears.length; i++){
+  for(let i = 1; i <= humanYears; i++){
     if (i === 1) {
       catYears += 15;
       dogYears += 15;
     } else if (i === 2) {
       catYears += 9;
       dogYears += 9;
-    } else if (i >= 3) {
+    } else (i >= 3) {
       catYears += 4;
       dogYears += 5;
     }
